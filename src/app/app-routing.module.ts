0c652c4aed0e688a6c793beeb9dbb72d7ff9eadc
Rita-Sonavane <sonavaners1999@gmail.com.com>
2024-04-21 23:08:17 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"home",pathMatch:'full'},
+  {path:"header",component:HeaderComponent},
+  // {path:"home",component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
