6f8cfbe5d6ee4fdd74e78ad272e0a797453fdcbb
Rita-Sonavane <sonavaners1999@gmail.com.com>
2024-04-22 14:15:05 +0530

    added footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path:"",redirectTo:"home",pathMatch:'full'},
   {path:"header",component:HeaderComponent},
-  // {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"home",component:HomeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
